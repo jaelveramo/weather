1e77db09e9d642cf0fabca64a2299c9b3bb66cde
unknown <jaelveramo@yahoo.es>
2023-02-18 14:59:33 -0500

    weather_20230218_1459

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -35,13 +35,13 @@ let loadWeekForecastData = () => {
     let [ciudad1] = weather_data;
 	let {city_code, city, date, maxtemperature, mintemperature, cloudiness, wind, rainfall, forecast_today, forecast_week, ...restoDeWeatherData} = ciudad1;
 
-    let listaDeElementos = document.getElementsByClassName('list-group');
+    let listaForescastWeek = document.getElementsByClassName('list-group');
+    let template_forecast = '';
 
-    for (i = 0; i < forecast_week.length; i++) {
-        let {day, text, date, temperature, icon} = forecast_week[i];
-        let {min, max} = temperature;
+    for (let i = 0; i < forecast_week.length; i++) {
+        let {day, text, date, temperature: {min, max}, icon} = forecast_week[i];
 
-        let template_forecast = `<li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+        template_forecast += `<li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
         <div class="d-flex flex-column">
           <h6 class="mb-1 text-dark font-weight-bold text-sm">${text}</h6>
           <span class="text-xs">${date}</span>
@@ -51,9 +51,9 @@ let loadWeekForecastData = () => {
           <div class="ms-4"><i class="material-icons fs-2 me-1 rainy">${icon}</i></div>
         </div>
       </li>`;
-    
-        listaDeElementos.push(template_forecast);
     };
+    
+    listaForescastWeek[0].innerHTML = template_forecast;
 };
 
 
