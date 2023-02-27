767f33fbb137937efffd31a29e8e6b81c79fdc6d
unknown <jaelveramo@yahoo.es>
2023-02-27 01:55:23 -0500

    weather_20230227_0144

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -44,7 +44,8 @@ let loadWeekForecastData = () => {
     for (let i = 0; i < forecast_week.length; i++) {
         let {day, text, date, temperature: {min, max}, icon} = forecast_week[i];
 
-        plantilla_pronostico += `<li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+        plantilla_pronostico += `
+        <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
         <div class="d-flex flex-column">
           <h6 class="mb-1 text-dark font-weight-bold text-sm">${text}</h6>
           <span class="text-xs">${date}</span>
